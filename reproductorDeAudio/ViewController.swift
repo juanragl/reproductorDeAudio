aa867f8e6fab53ba7ae7b820669102a44c1c7e55
juan ramon gonzalez <juanra@MacBook-Pro-de-juan.local>
2015-07-17 10:13:39 +0200

    Initial Commit

---
+++ reproductorDeAudio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reproductorDeAudio
+//
+//  Created by juan ramon gonzalez on 17/7/15.
+//  Copyright (c) 2015 juan ramon gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ reproductorDeAudioTests/reproductorDeAudioTests.swift
@@ -0,0 +1,36 @@
+//
+//  reproductorDeAudioTests.swift
+//  reproductorDeAudioTests
+//
+//  Created by juan ramon gonzalez on 17/7/15.
+//  Copyright (c) 2015 juan ramon gonzalez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class reproductorDeAudioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
